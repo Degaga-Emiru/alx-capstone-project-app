7f13d045f7ff87100e83dc9725f794d5158e3527
Degaga Emiru <degagaemiru@gmail.com>
2025-03-20 12:00:28 +0300

    create login page with local storage

--- book-library-app/src/pages/Signup.jsx
+++ book-library-app/src/pages/Signup.jsx
@@ -37,6 +37,7 @@ const Signup = () => {
   };
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        
       <div className="bg-white p-8 rounded-lg shadow-lg w-96">
         <h2 className="text-2xl font-bold mb-6 text-center">Signup</h2>
         <Formik
