285c5605d3211a08651b8c26049ecaf45e360ef8
Degaga Emiru <degagaemiru@gmail.com>
2025-03-20 09:55:16 +0300

    create book library react app

---
+++ book-library-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <div>
+      <h1 className='bg-slate-500 font-bold'> Hello react and tailwind </h1>
+      </div> 
+    </>
+  )
+}
+
+export default App
