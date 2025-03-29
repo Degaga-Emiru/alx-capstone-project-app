c6098cd7dd2831e6e0fc0246114efdf5fdd25411
Degaga Emiru <degagaemiru@gmail.com>
2025-03-29 08:29:55 +0300

    import the all components  in App jsx also used the Protected router for authenticated pages

--- book-library-app/src/App.jsx
+++ book-library-app/src/App.jsx
@@ -1,18 +1,69 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import WelcomePage from "./pages/welcomePage";
-import Signup from './pages/Signup';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import WelcomePage from './components/welcomePage';
+import Home from './components/Home';
+import BookDetails from './components/BookDetails';
+import Favorites from './components/Favorites';
+import ReadingList from './components/ReadingList';
+import BorrowedBooks from './components/BorrowedBooks';
+import SignUp from './pages/SignUp';
 import Login from './pages/Login';
-function App() {
+import useAuthStore from './store/store';
+
+const App = () => {
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+
+  // Protect routes that require authentication
+  const ProtectedRoute = ({ children }) => {
+    if (!isLoggedIn) {
+      return <Navigate to="/login" />;
+    }
+    return children;
+  };
+
   return (
     <Router>
       <Routes>
+        {/* Welcome Page (Public) */}
         <Route path="/" element={<WelcomePage />} />
-        <Route path="/signup" element={<Signup />} />
+        {/* Home Page (Protected) */}
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute> } />
+        <Route
+          path="/book-details"
+          element={
+            <ProtectedRoute>
+              <BookDetails />
+            </ProtectedRoute>}/>
+        <Route
+          path="/favorites"
+          element={
+            <ProtectedRoute>
+              <Favorites />
+            </ProtectedRoute>
+          }/>
+        <Route
+          path="/reading-list"
+          element={
+            <ProtectedRoute>
+              <ReadingList />
+            </ProtectedRoute> }/>
+        <Route
+          path="/borrowed-books"
+          element={
+            <ProtectedRoute>
+              <BorrowedBooks />
+            </ProtectedRoute>
+          }/>
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
